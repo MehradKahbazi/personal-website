298b82969993702f0c5ecdaff65e41f4e7ca6a4e
Mehrad <mehradhetfield@gmail.com>
2024-06-17 20:57:36 +0330

    feat: first panel implemented

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,6 +3,9 @@ import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import { useState } from 'react';
 import CustomTabPanel from './CustomTabPanel';
+import { Divider, Grid, Typography } from '@mui/material';
+import sampleImage from "../assets/paella.jpg";
+
 
 
 
@@ -30,7 +33,15 @@ export default function BasicTabs() {
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        Item One
+        <Grid container justifyContent='space-between'>
+            <Grid item sm={12} lg={5}>
+                <img src={sampleImage} />
+            </Grid>
+                <Divider orientation="vertical" flexItem sx={{ ml: "-50px" }}/>
+            <Grid item sm={12} lg={7}>
+                <Typography variant='h5'>about me</Typography>
+            </Grid>
+        </Grid>
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         Item Two
--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 import {
-    Typography,
-    AppBar,
-    CssBaseline,
-    Toolbar,
-    IconButton,
-    Drawer, 
-    Container
-  } from "@mui/material";
-  import MenuIcon from '@mui/icons-material/Menu';
+  Typography,
+  AppBar,
+  CssBaseline,
+  Toolbar,
+  IconButton,
+  Drawer,
+  Container,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 import DrawerList from "../components/DrowerList";
 import Footer from "../components/Footer";
@@ -20,22 +20,26 @@ const MainLayout = () => {
     setOpen(newOpen);
   };
   return (
-    <Container sx={{minHeight: '100vh'}}>
+    <>
       <CssBaseline />
-      <AppBar position="sticky">
-        <Toolbar variant="myVariant">
-          <Typography variant="h6" marginLeft={2}>
-            My Personal website
-          </Typography>
-          <IconButton><MenuIcon onClick={toggleDrawer(true)} /></IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        <DrawerList toggleDrawer={toggleDrawer} />
-      </Drawer>
-      <Outlet />
-      <Footer />
-    </Container>
+      <Container sx={{ minHeight: "100vh" }}>
+        <AppBar position="sticky">
+          <Toolbar variant="myVariant">
+            <Typography variant="h6" marginLeft={2}>
+              My Personal website
+            </Typography>
+            <IconButton>
+              <MenuIcon onClick={toggleDrawer(true)} />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          <DrawerList toggleDrawer={toggleDrawer} />
+        </Drawer>
+        <Outlet />
+        <Footer />
+      </Container>
+    </>
   );
 };
 
