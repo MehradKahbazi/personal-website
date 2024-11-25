b381d0ee4ed148e83a585c6594854a0d755a2847
Mehrad <mehradhetfield@gmail.com>
2024-11-25 20:22:53 +0330

    chore: footer sections

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,20 @@
 const Footer = () => {
     return ( 
         <footer className="container-fluid bg-secondary" style={{height: '35vh'}}>
-
+            <section className="row h-50">
+                sample
+            </section>
+            <section className="row h-50 align-items-end justify-content-center">
+                <div className="col-lg-11 border-top h-100 d-flex flex-column justify-content-center">
+                    <h1 className="text-center">Mehrad Kahbazi</h1>
+                    <small className="text-light text-center d-block">Front-End Developer</small>
+                </div>
+            </section>
+            <section className="row">
+                <div className="col-12 bg-black">
+                    <small className="d-block text-center">All Rights reserved by Mehrad Kahbazi® 2024</small>
+                </div>
+            </section>
         </footer>
      );
 }
