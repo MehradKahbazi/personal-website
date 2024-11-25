85be557e569fc9b3b7fbf98ea37eb4188ef2c2f1
Mehrad <mehradhetfield@gmail.com>
2024-11-25 20:31:58 +0330

    fix: footer structure fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,17 +4,16 @@ const Footer = () => {
             <section className="row h-50">
                 sample
             </section>
-            <section className="row h-50 align-items-end justify-content-center">
+            <section className="row align-items-end justify-content-center h-50">
                 <div className="col-lg-11 border-top h-100 d-flex flex-column justify-content-center">
                     <h1 className="text-center">Mehrad Kahbazi</h1>
                     <small className="text-light text-center d-block">Front-End Developer</small>
                 </div>
-            </section>
-            <section className="row">
-                <div className="col-12 bg-black">
-                    <small className="d-block text-center">All Rights reserved by Mehrad Kahbazi® 2024</small>
+                <div className="col-lg-12 bg-black">
+                    <small className="d-block text-center text-light font-weight-lighter">All Rights reserved by Mehrad Kahbazi® 2024</small>
                 </div>
             </section>
+            
         </footer>
      );
 }
